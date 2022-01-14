8fd7336dde431a238041b488ab9b0c2657f46647
ferion0204 <dltjdgh@konkuk.ac.kr>
2022-01-14 22:54:37 +0900

    boardDAO using mybatis

--- main/java/board/dao/BoardDAO.java
+++ main/java/board/dao/BoardDAO.java
@@ -1,29 +1,21 @@
 package board.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
+import java.io.IOException;
+import java.io.Reader;
 import java.util.List;
 import java.util.Map;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.sql.DataSource;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import board.bean.BoardDTO;
 
 public class BoardDAO {
-	private Connection conn;
-	private PreparedStatement pstmt;
-	private ResultSet rs;
-
 	private static BoardDAO instance;
-
-	private DataSource ds;
-
+	private SqlSessionFactory sqlSessionFactory;
+	
 	public static BoardDAO getInstance() {
 		synchronized (BoardDAO.class) {
 			if (instance == null) {
@@ -36,192 +28,48 @@ public class BoardDAO {
 
 	public BoardDAO() {
 		try {
-			Context context = new InitialContext();
-			ds = (DataSource) context.lookup("java:comp/env/jdbc/oracle");// Tomcat의 경우
-
-		} catch (NamingException e) {
+			Reader reader = Resources.getResourceAsReader("mybatis-config.xml");
+			sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 	}
 
 	public void boardWrite(Map<String, String> map) {
-		String sql = "insert into board(seq,id,name,email,subject,content,ref) values(seq_board.nextval,?,?,?,?,?,seq_board.currval)";
-		try {
-			conn = ds.getConnection();
-			pstmt=conn.prepareStatement(sql);
-			
-			pstmt.setString(1, map.get("id"));
-			pstmt.setString(2, map.get("name"));
-			pstmt.setString(3, map.get("email"));
-			pstmt.setString(4, map.get("subject"));
-			pstmt.setString(5, map.get("content"));
-			
-			
-			pstmt.executeUpdate();//실행-리턴하는 값은 영향을 받은 갯수(int)
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}finally {
-			try {
-				if (pstmt != null)
-					pstmt.close();
-				if (conn != null)
-					conn.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
+		SqlSession sqlSession=sqlSessionFactory.openSession();
+		sqlSession.update("boardSQL.boardWrite", map);
+		sqlSession.commit();
+		sqlSession.close();
 	}
 
 	public List<BoardDTO> getBoardList(Map<String, Integer> map) {
-		
-		String sql="select * from (select rownum rn,tt.* from "
-				+ "(select seq,id,name,email,subject,content,ref,lev,step,pseq,reply,hit,"
-				+ "to_char(logtime,'yyyy.mm.dd') as \"logtime\" from board order by ref desc, step asc) tt"
-				+ ")where rn>=? and rn<=?";
-		List<BoardDTO> list = new ArrayList<BoardDTO>();
-		
-		try {
-			conn=ds.getConnection();
-			pstmt=conn.prepareStatement(sql);
-			pstmt.setInt(1, map.get("startNum"));
-			pstmt.setInt(2, map.get("endNum"));
-			
-			rs=pstmt.executeQuery();
-			
-			while(rs.next()) {
-				BoardDTO boardDTO=new BoardDTO();
-				
-				boardDTO.setSeq(rs.getInt("seq"));
-				boardDTO.setId(rs.getString("id"));
-				boardDTO.setName(rs.getString("name"));
-				boardDTO.setEmail(rs.getString("email"));
-				boardDTO.setSubject(rs.getString("subject"));
-				boardDTO.setContent(rs.getString("content"));
-				boardDTO.setRef(rs.getInt("ref"));
-				boardDTO.setLev(rs.getInt("lev"));
-				boardDTO.setStep(rs.getInt("step"));
-				boardDTO.setPseq(rs.getInt("pseq"));
-				boardDTO.setReply(rs.getInt("reply"));
-				boardDTO.setHit(rs.getInt("hit"));
-				boardDTO.setLogtime(rs.getString("logtime"));
-				
-				list.add(boardDTO);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-			list=null;
-		} finally {
-			try {
-				if (rs!=null)
-					rs.close();
-				if (pstmt != null)
-					pstmt.close();
-				if (conn != null)
-					conn.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
+		SqlSession sqlSession=sqlSessionFactory.openSession();
+		List<BoardDTO> list=sqlSession.selectList("boardSQL.getBoardList", map);
+		sqlSession.close();
 		return list;
 	}
 
 	public BoardDTO selectOne(int seq) {
-		BoardDTO boardDTO=null;
-		String sql="select seq,id,name,email,subject,content,ref,lev,step,pseq,reply,hit,"
-				+ "to_char(logtime,'yyyy.mm.dd') as \"logtime\" from board where seq=?";
-		
-		try {
-			conn=ds.getConnection();
-			pstmt=conn.prepareStatement(sql);
-			pstmt.setInt(1, seq);
-			rs=pstmt.executeQuery();
-			
-			if(rs.next()) {
-				boardDTO=new BoardDTO();
-				
-				boardDTO.setSeq(rs.getInt("seq"));
-				boardDTO.setId(rs.getString("id"));
-				boardDTO.setName(rs.getString("name"));
-				boardDTO.setEmail(rs.getString("email"));
-				boardDTO.setSubject(rs.getString("subject"));
-				boardDTO.setContent(rs.getString("content"));
-				boardDTO.setRef(rs.getInt("ref"));
-				boardDTO.setLev(rs.getInt("lev"));
-				boardDTO.setStep(rs.getInt("step"));
-				boardDTO.setPseq(rs.getInt("pseq"));
-				boardDTO.setReply(rs.getInt("reply"));
-				boardDTO.setHit(rs.getInt("hit"));
-				boardDTO.setLogtime(rs.getString("logtime"));
-				
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (rs!=null)
-					rs.close();
-				if (pstmt != null)
-					pstmt.close();
-				if (conn != null)
-					conn.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
+		SqlSession sqlSession=sqlSessionFactory.openSession();
+		BoardDTO boardDTO=sqlSession.selectOne("boardSQL.getBoardView",seq);
+		sqlSession.close();
 		return boardDTO;
 	}
 	
 	public int getTotalA() {
-		int totalA=0;
-		String sql="select count(*) as total from board";
-		try {
-			conn=ds.getConnection();
-			pstmt=conn.prepareStatement(sql);
-			rs=pstmt.executeQuery();
-			if(rs.next()) {
-				totalA=rs.getInt("total");
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (rs!=null)
-					rs.close();
-				if (pstmt != null)
-					pstmt.close();
-				if (conn != null)
-					conn.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
+		SqlSession sqlSession=sqlSessionFactory.openSession();
+		int totalA=sqlSession.selectOne("boardSQL.getTotalA");
+		sqlSession.close();
 		return totalA;
 	}
 
 	public int boardModify(BoardDTO boardDTO) {
-		int result=0;
-		String sql="update board set subject=?, content=? where seq=?";
-		try {
-			conn=ds.getConnection();
-			pstmt=conn.prepareStatement(sql);
-			pstmt.setString(1, boardDTO.getSubject());
-			pstmt.setString(2, boardDTO.getContent());
-			pstmt.setInt(3, boardDTO.getSeq());
-			result=pstmt.executeUpdate();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (pstmt != null)
-					pstmt.close();
-				if (conn != null)
-					conn.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		
+		SqlSession sqlSession=sqlSessionFactory.openSession();
+		int result=sqlSession.update("boardSQL.boardModify",boardDTO);
+		sqlSession.commit();
+		sqlSession.close();
 		return result;
 	}
 
